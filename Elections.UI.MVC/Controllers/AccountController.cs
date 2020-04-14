451abb8b46b75a1a65ffe321a0033308fcaff4c6
abhiramvikrant <abhiramvikrant@gmail.com>
2020-04-14 09:35:18 +0530

    added users to roles

--- Elections.UI.MVC/Controllers/AccountController.cs
+++ Elections.UI.MVC/Controllers/AccountController.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Authorization;
 using ElectionsIndia.DataAccess.UserFields;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using ElectionsIndia.Models.ViewModels;
 
 namespace Elections.UI.MVC.Controllers
 {
@@ -22,16 +24,16 @@ namespace Elections.UI.MVC.Controllers
         private readonly SignInManager<ApplicationUser> smanager;
         private readonly RoleManager<IdentityRole> rmanager;
         private readonly IMapper mapper;
-        
+
 
         // GET: Account
-        public AccountController(UserManager<ApplicationUser> umanager,SignInManager<ApplicationUser> smanager,  RoleManager<IdentityRole> rmanager, IMapper mapper)
+        public AccountController(UserManager<ApplicationUser> umanager, SignInManager<ApplicationUser> smanager, RoleManager<IdentityRole> rmanager, IMapper mapper)
         {
             this.umanager = umanager;
             this.smanager = smanager;
             this.rmanager = rmanager;
             this.mapper = mapper;
-           
+
         }
         [HttpGet]
         [AllowAnonymous]
@@ -42,53 +44,53 @@ namespace Elections.UI.MVC.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-       
+
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
                 var user = mapper.Map<ApplicationUser>(model);
-              
-            var result = await smanager.PasswordSignInAsync(model.UserName, model.Password, true, false).ConfigureAwait(true);
-               
-            
+
+                var result = await smanager.PasswordSignInAsync(model.UserName, model.Password, true, false).ConfigureAwait(true);
+
+
 
                 if (result.Succeeded)
                 {
-                    
-                  
-                    var u = await smanager.UserManager.FindByEmailAsync(model.UserName).ConfigureAwait(true);                                
-                   
-                        var claims = new List<Claim>();
-                        
-                        claims.Add(new Claim(ClaimTypes.Name, u.UserName));
+
+
+                    var u = await smanager.UserManager.FindByEmailAsync(model.UserName).ConfigureAwait(true);
+
+                    var claims = new List<Claim>();
+
+                    claims.Add(new Claim(ClaimTypes.Name, u.UserName));
                     claims.Add(new Claim(ClaimTypes.Email, u.Email));
-           
-                        var userIdentity = new ClaimsIdentity("Custom");
-                        userIdentity.AddClaims(claims);
-                        ClaimsPrincipal uPrincipal = new ClaimsPrincipal(userIdentity);
-                        AuthenticationProperties ap = new AuthenticationProperties()
-                        {
-                            ExpiresUtc = DateTime.UtcNow.AddDays(1),
-                            IsPersistent = model.RememberMe,
-                            AllowRefresh = false
-                        };
 
-                        await smanager.SignInWithClaimsAsync(u, ap, claims).ConfigureAwait(true);
+                    var userIdentity = new ClaimsIdentity("Custom");
+                    userIdentity.AddClaims(claims);
+                    ClaimsPrincipal uPrincipal = new ClaimsPrincipal(userIdentity);
+                    AuthenticationProperties ap = new AuthenticationProperties()
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddDays(1),
+                        IsPersistent = model.RememberMe,
+                        AllowRefresh = false
+                    };
 
-                        ViewBag.Auth = User.Identity.IsAuthenticated;
-                    }
-                    //return LocalRedirect($"~/account/login?handler=SetIdentity");
+                    await smanager.SignInWithClaimsAsync(u, ap, claims).ConfigureAwait(true);
 
-                   
-                   
+                    ViewBag.Auth = User.Identity.IsAuthenticated;
+                }
+                //return LocalRedirect($"~/account/login?handler=SetIdentity");
 
-                    return RedirectToAction("index", "languages");
-                
+
+
+
+                return RedirectToAction("index", "languages");
+
             }
             return View();
         }
-       
+
         public async Task<IActionResult> Logout()
         {
             await smanager.SignOutAsync().ConfigureAwait(true);
@@ -118,10 +120,10 @@ namespace Elections.UI.MVC.Controllers
                 if (result.Succeeded)
                 {
                     var re = await umanager.AddPasswordAsync(user, model.Password).ConfigureAwait(true);
-                    return RedirectToAction("index","languages");
+                    return RedirectToAction("index", "languages");
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return View();
             }
@@ -151,7 +153,7 @@ namespace Elections.UI.MVC.Controllers
             }
         }
 
-      
+
 
         // POST: Account/Delete/5
         [HttpPost]
@@ -172,8 +174,63 @@ namespace Elections.UI.MVC.Controllers
             }
         }
 
+
+
         [HttpGet]
+        public async Task<IActionResult> AddUsersToRoles(string RoleId)
+        {
+            var role = rmanager.Roles.Where(r => r.Id == RoleId).FirstOrDefault();
+            var userNotInRoles = umanager.Users.ToList();
+            List<UsersNotInRolesViewModel> UserNotRoleList = new List<UsersNotInRolesViewModel>();
+
+            foreach (var item in userNotInRoles)
+            {
+                UserNotRoleList.Add(new UsersNotInRolesViewModel()
+                {
+                    IsInRole = await umanager.IsInRoleAsync(item, role.Name).ConfigureAwait(true),
+                    RoleId = RoleId,
+                    UserId = item.Id,
+                    UserName = item.UserName
+                });
+            }
+
+            return View(UserNotRoleList);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUsersToRoles(IList<UsersNotInRolesViewModel> models, string RoleId)
+        {
+            var role = rmanager.Roles.Where(r => r.Id == models[0].RoleId).FirstOrDefault();
+            foreach (var item in models)
+            {
+                var u = await umanager.FindByIdAsync(item.UserId).ConfigureAwait(true);
+                if ((item.IsInRole) && await umanager.IsInRoleAsync(u, role.Name).ConfigureAwait(true) == false)
+                {//if IsInRole is checked and not in roles. Add the user to roles.
+                    var user = await umanager.FindByIdAsync(item.UserId).ConfigureAwait(true);
+                    var result = await umanager.AddToRoleAsync(user, role.Name).ConfigureAwait(true);
+                }
+                else if ((!item.IsInRole) && await umanager.IsInRoleAsync(u, role.Name).ConfigureAwait(true))
+                {
+                    // If IsInRole is not checked and is in Role. Remove the user frm Role
+                    var user = await umanager.FindByIdAsync(item.UserId).ConfigureAwait(true);
+                    var result = await umanager.RemoveFromRoleAsync(user, role.Name).ConfigureAwait(true);
+                }
+
+            }
+
+
+
+
+            return RedirectToAction("AddUsersToRoles", new { RoleId = role.Id });
+        }
+
+
+       [HttpGet]
         public IActionResult CreateRole()
+        {
+            return GetRoleList();
+        }
+
+        private IActionResult GetRoleList()
         {
             var rolelist = rmanager.Roles.ToList();
             RoleCreateViewModels model = new RoleCreateViewModels();
@@ -182,12 +239,17 @@ namespace Elections.UI.MVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateRoles(string rolename)
+        public async Task<IActionResult> CreateRole(string rolename)
         {
             IdentityRole role = new IdentityRole(rolename);
             var result = await rmanager.CreateAsync(role).ConfigureAwait(true);
-           return  RedirectToAction("createrole");
-
+            return GetRoleList();
+            
         }
+
+    
+
+
+
     }
 }
---
+++ EletionsIndia.Models/ViewModels/UsersNotInRolesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ElectionsIndia.Models.ViewModels
+{
+    public class UsersNotInRolesViewModel
+    {
+        public string UserId { get; set; }
+
+        public string RoleId { get; set; }
+        [Display(Name="Is In Role")]
+        public bool IsInRole { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+    }
+}
