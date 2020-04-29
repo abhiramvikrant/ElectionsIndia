9cd4b00b95c3c0cb6eac151093c67ce837686d52
abhiramvikrant <abhiramvikrant@gmail.com>
2020-04-29 13:54:56 +0530

    Added isUt to states

--- Elections.Live/dbo/Tables/States.sql
+++ Elections.Live/dbo/Tables/States.sql
@@ -3,8 +3,11 @@
     [Name]      NVARCHAR (100) NOT NULL,
     [IsActive]  BIT            CONSTRAINT [DF_States_IsActive] DEFAULT ((0)) NOT NULL,
     [CountryID] INT            NULL,
+    [IsUT]      BIT            CONSTRAINT [DF_States_IsUT] DEFAULT ((0)) NULL,
     CONSTRAINT [PK_States] PRIMARY KEY CLUSTERED ([StateID] ASC)
 );
+
+
 
 
 
